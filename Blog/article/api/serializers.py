0f7acc07cebcebd89d05a5e5f4f39c8cc8a52c25
Ban Vuong <vuongdieuban@gmail.com>
2018-11-04 23:31:05 -0700

    display author_name instead of ForeignKey number

--- Blog/article/api/serializers.py
+++ Blog/article/api/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from article.models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
+    # author is ForeignKey to user (django user), which has attr username, first_name, last_name
+    author_name = serializers.CharField(source='author.username', read_only=True)
     class Meta:
         model = Article
         fields = [
             'id',
-            'author',
+            'author_name',
             'title',
             'created',
             'description',
@@ -14,4 +16,4 @@ class ArticleSerializer(serializers.ModelSerializer):
             'slug',
         ]
 
-        read_only_fields = ['slug', 'author',]
+        read_only_fields = ['slug',]
